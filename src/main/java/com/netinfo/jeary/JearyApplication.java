5c7db2e2f3cf5ac1141ba1c29bef1ceddb2d3ebf
Garyjjjj <1263571997@qq.com>
2019-05-06 20:25:57 +0800

    first commit

---
+++ src/main/java/com/netinfo/jeary/JearyApplication.java
@@ -0,0 +1,13 @@
+package com.netinfo.jeary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JearyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JearyApplication.class, args);
+    }
+
+}
